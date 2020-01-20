d6b852bf3cfb5bdf08c50a9d89615ffacb901026
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-01-20 13:43:25 -0800

    created cart context wrapper

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route } from 'react-router-dom';
 import data from './data';
 
 import { ProductContext } from './contexts/ProductContext'
-
+import { CartContext } from './contexts/CartContext'
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -20,22 +20,23 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value = {{products, addItem}}>
-				<Navigation cart={cart} />
+				<CartContext.Provider value = {cart}>
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route
-					exact
-					path="/"
-					
-					component={Products}
-					
-				/>
+					{/* Routes */}
+					<Route
+						exact
+						path="/"
+						
+						component={Products}
+						
+					/>
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
-
+					<Route
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
+					/>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
